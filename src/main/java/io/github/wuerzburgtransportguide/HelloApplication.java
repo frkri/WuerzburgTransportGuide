8a980d42b703fc75c82c4769143e88c71a0b9038
frkri <frkri@users.noreply.github.com>
2023-12-28 18:49:48 +0000

    Apply spotless formatting

--- src/main/java/io/github/wuerzburgtransportguide/HelloApplication.java
+++ src/main/java/io/github/wuerzburgtransportguide/HelloApplication.java
@@ -1,22 +1,24 @@
 package io.github.wuerzburgtransportguide;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application {
-  @Override
-  public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
-    stage.setScene(scene);
-    stage.show();
-  }
+import java.io.IOException;
 
-  public static void main(String[] args) {
-    launch();
-  }
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/io/github/wuerzburgtransportguide/client/ApiClient.java
+++ src/main/java/io/github/wuerzburgtransportguide/client/ApiClient.java
@@ -1,31 +1,34 @@
 package io.github.wuerzburgtransportguide.client;
 
-import java.nio.file.Path;
-import java.util.List;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+
 import retrofit2.Retrofit;
 
+import java.nio.file.Path;
+import java.util.List;
+
 public class ApiClient {
-  private final Retrofit retrofitClient;
+    private final Retrofit retrofitClient;
 
-  public ApiClient(String apiUrl) {
-    var cacheDir =
-        Path.of(System.getenv("AppData"), "local", "wuerzburgtransportguide", "cache").toFile();
-    var cacheStore = new Cache(cacheDir, 10L * 1024L * 1024L);
-    var cookieStore = new FilteredCookieStore(List.of("XSRF-TOKEN"));
+    public ApiClient(String apiUrl) {
+        var cacheDir =
+                Path.of(System.getenv("AppData"), "local", "wuerzburgtransportguide", "cache")
+                        .toFile();
+        var cacheStore = new Cache(cacheDir, 10L * 1024L * 1024L);
+        var cookieStore = new FilteredCookieStore(List.of("XSRF-TOKEN"));
 
-    var okHttpClient =
-        new OkHttpClient.Builder()
-            .cache(cacheStore)
-            .cookieJar(cookieStore)
-            .followRedirects(false)
-            .retryOnConnectionFailure(true)
-            .build();
-    retrofitClient = new Retrofit.Builder().baseUrl(apiUrl).client(okHttpClient).build();
-  }
+        var okHttpClient =
+                new OkHttpClient.Builder()
+                        .cache(cacheStore)
+                        .cookieJar(cookieStore)
+                        .followRedirects(false)
+                        .retryOnConnectionFailure(true)
+                        .build();
+        retrofitClient = new Retrofit.Builder().baseUrl(apiUrl).client(okHttpClient).build();
+    }
 
-  public <T> T createClient(Class<T> clientInterface) {
-    return retrofitClient.create(clientInterface);
-  }
+    public <T> T createClient(Class<T> clientInterface) {
+        return retrofitClient.create(clientInterface);
+    }
 }
