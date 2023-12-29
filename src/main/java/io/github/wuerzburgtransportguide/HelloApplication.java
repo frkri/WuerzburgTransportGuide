3c36cca18a99d5d73327ef311e10273331f3d293
Wild-Rage <57220519+frkri@users.noreply.github.com>
2023-12-30 00:28:49 +0100

    Update layout of pages

--- src/main/java/io/github/wuerzburgtransportguide/HelloApplication.java
+++ src/main/java/io/github/wuerzburgtransportguide/HelloApplication.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainpage.fxml"));
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(HelloApplication.class.getResource("pages/start/start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
