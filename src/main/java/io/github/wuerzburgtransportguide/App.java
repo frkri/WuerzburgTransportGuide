f7bcc3f17a661b2cb1472bd19223df4a8960728d
Wild-Rage <57220519+frkri@users.noreply.github.com>
2024-01-02 02:31:04 +0100

    Update start page and add global css file

--- src/main/java/io/github/wuerzburgtransportguide/App.java
+++ src/main/java/io/github/wuerzburgtransportguide/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("pages/start/start.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Wuerzburg Transport Guide");
         stage.setScene(scene);
         stage.show();
