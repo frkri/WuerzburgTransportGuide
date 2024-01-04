b4d225b6421997bb8e3ee0af957a8b3f3e5feabf
Wild-Rage <57220519+frkri@users.noreply.github.com>
2024-01-04 02:52:15 +0100

    Fix modules opens

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module io.github.wuerzburgtransportguide {
     requires javafx.controls;
     requires javafx.fxml;
-
-    // Dependencies for OpenAPI client
     requires retrofit2;
     requires okhttp3;
+    requires retrofit2.converter.gson;
     requires com.google.gson;
     requires jakarta.annotation;
     requires org.jetbrains.annotations;
 
     opens io.github.wuerzburgtransportguide to
             javafx.fxml;
+    opens io.github.wuerzburgtransportguide.model to
+            com.google.gson;
 
     exports io.github.wuerzburgtransportguide;
 }
